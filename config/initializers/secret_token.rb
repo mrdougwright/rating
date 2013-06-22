20127436639574188d92953004739984fa30ce5a
Doug Wright <mrdougwright@gmail.com>
2013-06-22 14:16:08 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rating::Application.config.secret_token = '66fe7332ee493b38c9f9feabedc33d0f6b7751e47f1d1c848e6a8eab178a519061a6d6df537f862f408d76408e8c993ff3cdbac2cfa9ad12b6754f28a1dcdbad'
